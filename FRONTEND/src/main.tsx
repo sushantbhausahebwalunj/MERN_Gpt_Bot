36100bc7bb2cc28eef9e5b999c1a84bd2820a198
sushantbhausahebwalunj <sushantwalunj2001@gmail.com>
2024-06-30 02:02:26 +0530

    backend hosted link updatedd in frontrnd main.tsx

--- FRONTEND/src/main.tsx
+++ FRONTEND/src/main.tsx
@@ -7,7 +7,8 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
 import { Toaster } from 'react-hot-toast'
 import axios from 'axios' //axios package is used connecting fronted with backend
-axios.defaults.baseURL = "http://localhost:5000/api/v1" // here to pass our base url as the main url  of our api server 
+// axios.defaults.baseURL = "http://localhost:5000/api/v1" // here to pass our base url as the main url  of our api server 
+axios.defaults.baseURL= `http://merngptbot-production.up.railway.app`
 axios.defaults.withCredentials = true //withCredential is use to exchange the cookie  information between client and server 
 
 const theme = createTheme({typography:{fontFamily:'Roboto Slab, serif', allVariants:{color:"white"},}})//this is provided by mui
