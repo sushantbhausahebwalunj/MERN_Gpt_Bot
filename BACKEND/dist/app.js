ceaaee25776fd2bac530eb7801b8ff2f33834a81
sushantbhausahebwalunj <sushantwalunj2001@gmail.com>
2024-06-30 11:09:51 +0530

    unpadeted frontend hosted url to backend and made chnages n frontend main.ts in http....

--- BACKEND/dist/app.js
+++ BACKEND/dist/app.js
@@ -9,7 +9,7 @@ const app = express();
 //middleware
 //middlewares are the functions which handle the request
 // app.use is use to definr a middleware 
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); //enable CORS for all requests and connect frontend local host to backend
+app.use(cors({ origin: `https://mern-gpt-bot-1.onrender.com`, credentials: true })); //enable CORS for all requests and connect frontend local host to backend
 app.use(express.json()); // for parsing application/json
 app.use(cookieParser(process.env.COOKIE_SECRET)); //for handling cookies this will generate the cookiee
 // use while only development mode once we  deploy application it will be removed
